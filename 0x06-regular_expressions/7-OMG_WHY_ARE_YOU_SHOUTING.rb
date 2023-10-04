e636d7ff1376f550eb792b3b0fc9d4d204f27671
Nii Armah Aryeetey <kobbyaryeetey@gmail.com>
2023-10-04 03:16:10 +0000

    Update 7-OMG_WHY_ARE_YOU_SHOUTING.rb

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,8 +1,2 @@
-#!/usr/bin/env bash
-#bash script that displays 'To infinity and beyond', if kill attempt 'I am invincible!!!'
-trap 'echo "I am invincible!!!"' SIGTERM
-counter=2
-while [ $counter -gt 0 ]; do
-    echo "To infinity and beyond"
-    sleep 2
-done
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/[A-Z]+/).join
